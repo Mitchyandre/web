69c6437aa8e164068d79370481202e68319319a6
Mitchyandre <mitchellramsey54@gmail.com>
2024-11-22 03:08:29 -0400

    Delete JS.js

--- JS.js
+++
@@ -1,6 +0,0 @@
- <script>
-        window.addEventListener('load', function() {
-            document.getElementById('loading').style.display = 'none';
-            document.getElementById('gallery').style.display = 'grid';
-        });
-    </script>
